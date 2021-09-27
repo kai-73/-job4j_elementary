76d747f8f1d2fb947461f859aba3f60ae2c475a9
kai-73 <oroman@rambler.ru>
2021-09-27 22:55:06 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,WWWworld");
+        System.out.println("Hello,WWworld");
     }
 }
