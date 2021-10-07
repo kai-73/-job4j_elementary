1f90f51aca8dcd655a089cd37e097e661f6c112c
kai-73 <oroman@rambler.ru>
2021-10-08 00:14:09 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -1,12 +1,11 @@
 package ru.job4j.calculator;
-
 public class MathFunc {
 
     public static int func1(int x) {
         int y = x * x + 1;
         return y;
     }
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+
     public static float func2(int x) {
         float y = 1 / x;
         return y;
