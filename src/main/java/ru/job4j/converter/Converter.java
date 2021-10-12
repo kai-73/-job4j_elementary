1fc612a6370b4a172a3abed938d360c417980ffd
kai-73 <oroman@rambler.ru>
2021-10-12 19:19:14 +0300

    5. Конвертер валюты | [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -6,17 +6,17 @@ public class Converter {
 
     public static float rubletoeuro(float value) {
         float rsl = value / 70;
-        return rsl;
+        return (float) Math.round(rsl * 100) / 100;
     }
 
     public static float rubletodollar(float value) {
         float rsl = value / 60;
-        return Math.round(rsl);
+        return (float) Math.round(rsl * 100) / 100;
     }
 
     public static float dollartoeuro(float value) {
-        float rsl = value * 0.86f;
-        return Math.round(rsl);
+        float rsl = value * 0.86F;
+        return (float) Math.round(rsl * 100) / 100;
     }
 
     public static void main(String[] args) {
@@ -24,8 +24,8 @@ public class Converter {
         float euro = Converter.rubletoeuro(value);
         float dollar = Converter.rubletodollar(140);
         float dollartoeuro = Converter.dollartoeuro(euro);
-        System.out.println("Euro: " + value + " " + euro);
-        System.out.println("Dollar:" + dollar);
-        System.out.println("DollarToEuro:" + dollartoeuro);
+        System.out.println("Exchange " + value + " Rub as " + euro + " Euro");
+        System.out.println("Exchange " + value + " Rub as " + dollar + " Dollar");
+        System.out.println("Exchange " + dollar + " Dollar as " + dollartoeuro + " Euro");
     }
 }
