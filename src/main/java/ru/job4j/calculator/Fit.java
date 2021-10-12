999b1f7eeb2c40dd8acc21b60b13e84d5d03f9ea
kai-73 <oroman@rambler.ru>
2021-10-13 01:48:50 +0300

    6. Идеальный вес [#41584 #215319]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,8 +2,8 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(short height) {
-        return (height - 100) * 1.15;
+    public static double manWeight(short heightm) {
+        return (heightm - 100) * 1.15;
     }
 
     public static double womanWeight(short heightw) {
@@ -11,11 +11,11 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short heightm = 187;
         short heightw = 172;
-        double man = Fit.manWeight(height);
+        double man = Fit.manWeight(heightm);
         double woman = Fit.womanWeight(heightw);
-        System.out.println("Man " + height + " cm is " + man + " kg");
+        System.out.println("Man " + heightm + " cm is " + man + " kg");
         System.out.println("Man " + heightw + " cm is " + woman + " kg");
     }
 }
