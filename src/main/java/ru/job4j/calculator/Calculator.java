d032ad51a2e2ee025bcbba3833efef9af2dfc0e6
kai-73 <oroman@rambler.ru>
2021-10-19 19:50:13 +0300

    reset branch

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,7 +5,6 @@ import static java.lang.System.lineSeparator;
 public class Calculator {
 
     public static void main(String[] args) {
-        int one = 1;
         int two = 2;
         int six = 6;
         int four = 4;
--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,23 +2,23 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static float rubletoeuro(float value1) {
+    public static float rubleToEuro(float value1) {
         return value1 / 70;
     }
 
-    public static float rubletodollar(float value1) {
+    public static float rubleToDollar(float value1) {
         return value1 / 60;
     }
 
-    public static float dollartoeuro(float value1) {
+    public static float dollarToEuro(float value1) {
         return value1 * 0.86F;
     }
 
     public static void main(String[] args) {
         float value1 = 140;
-        float euro = Converter.rubletoeuro(value1);
-        float dollar = Converter.rubletodollar(140);
-        float dollartoeuro = Converter.dollartoeuro(euro);
+        float euro = Converter.rubleToEuro(value1);
+        float dollar = Converter.rubleToDollar(140);
+        float dollartoeuro = Converter.dollarToEuro(euro);
         System.out.println("Exchange " + value1 + " Rub as " + euro + " Euro");
         System.out.println("Exchange " + value1 + " Rub as " + dollar + " Dollar");
         System.out.println("Exchange " + dollar + " Dollar as " + dollartoeuro + " Euro");
