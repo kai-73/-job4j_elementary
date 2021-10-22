0a2f32be0ee6c364fe3948a2b531a221b2f20017
kai-73 <oroman@rambler.ru>
2021-10-22 21:27:28 +0300

    0. Операции сравнения [#235631]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,8 +2,9 @@ package ru.job4j.condition;
 
 public class Liken {
 
-    public static void main(String[] args) {
-        int first = 10;
+    public static void main(String[] args)  {
+        int first;
+        first = 10;
         int second = 9;
 
         boolean result = first > second;
