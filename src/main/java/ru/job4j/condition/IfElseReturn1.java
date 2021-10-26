bee73bd596aad6da9ce5eb37c3ba253e64ef3b06
kai-73 <oroman@rambler.ru>
2021-10-26 20:17:27 +0300

    4.3.4. if c return [#333785] - 1

---
+++ src/main/java/ru/job4j/condition/IfElseReturn1.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class IfElseReturn1 {
+
+    public static boolean greatThen(int first, int second) {
+        boolean compare = first > second;
+        return compare;
+    }
+
+    public static void main(String[] args) {
+        boolean result = greatThen(10, 2);
+        System.out.println(result);
+        System.out.println(IfElseReturn1.greatThen(10, 34));
+    }
+}
