43382b437e260e36dfc7de89e0bd8488d8ecf4fe
kai-73 <oroman@rambler.ru>
2021-11-02 10:53:01 +0300

    5.4. Протеиновая диета [#156313 #223479]

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -35,7 +35,7 @@ public class FitnessTest {
 
     @Test
     public void whenIvan90EqualsNik90() {
-        int ivan = 90;
+        int ivan =  90;
         int nik = 90;
         int rsl = calc(ivan, nik);
         int expected = 0;
