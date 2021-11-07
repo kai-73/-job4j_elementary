c782acc54e7dbb25bcea6c2bfe7ad2d0935a4caf
kai-73 <oroman@rambler.ru>
2021-11-07 11:20:24 +0300

    6.4. Слово начинается с ... [#41585 #225441]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class ArrayChar {
 
-    public static boolean startsWitch(char[] word, char[] pref) {
+    public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
             if (pref[index] != word[index]) {
--- src/test/java/ru/job4j/array/ArrayCharTest.java
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -3,15 +3,13 @@ package ru.job4j.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ArrayCharTest {
 
     @Test
     public void whenStartWithPrefixThenTrue() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
         char[] pref = {'H', 'e'};
-        boolean result = ArrayChar.startsWitch(word, pref);
+        boolean result = ArrayChar.startsWith(word, pref);
         Assert.assertTrue(result);
     }
 
@@ -19,7 +17,7 @@ public class ArrayCharTest {
     public void whenNoStartWithPrefixThenFalse() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
         char[] pref = {'H', 'i'};
-        boolean result = ArrayChar.startsWitch(word, pref);
+        boolean result = ArrayChar.startsWith(word, pref);
         Assert.assertFalse(result);
     }
 }
