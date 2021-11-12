dfe1f755c6ebeacf1582ce95502ae7e5bee91d97
kai-73 <oroman@rambler.ru>
2021-11-12 23:18:39 +0300

    6.7.1. Моно строка в матрице. [#214126 #227227]

--- src/main/java/ru/job4j/array/MatrixChek.java
+++ src/main/java/ru/job4j/array/MatrixChek.java
@@ -3,14 +3,18 @@ package ru.job4j.array;
 public class MatrixChek {
     public static boolean monoHorizontal(char[][] board, int row) {
         for (int i = 0; i < board[row].length; i++) {
-            return board[row][i] == 'X';
+            if (board[row][i] != 'X') {
+                return false;
+            }
         }
         return true;
     }
 
     public static boolean monoVertical(char[][] board, int columm) {
         for (int i = 0; i < board[columm].length; i++) {
-            return board[i][columm] == 'X';
+            if (board[i][columm] != 'X') {
+                return false;
+            }
         }
         return true;
     }
--- src/test/java/ru/job4j/array/MatrixChekTest.java
+++ src/test/java/ru/job4j/array/MatrixChekTest.java
@@ -3,8 +3,6 @@ package ru.job4j.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MatrixChekTest {
 
     @Test
@@ -20,6 +18,18 @@ public class MatrixChekTest {
     }
 
     @Test
+    public void whenNotMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', ' ', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixChek.monoHorizontal(input, row);
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void whenHasMonoVertical() {
         char[][] input = {
                 {' ', ' ', 'X'},
@@ -29,6 +39,18 @@ public class MatrixChekTest {
         int column = 2;
         boolean result = MatrixChek.monoVertical(input, column);
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenHasMonoNoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+        };
+        int column = 2;
+        boolean result = MatrixChek.monoVertical(input, column);
+        Assert.assertFalse(result);
     }
 
     @Test
