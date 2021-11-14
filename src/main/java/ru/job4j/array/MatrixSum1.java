ab725aa1031dd39629bcd2502a7cb4cf54a05f31
kai-73 <oroman@rambler.ru>
2021-11-14 23:29:29 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741]

---
+++ src/main/java/ru/job4j/array/MatrixSum1.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class MatrixSum1 {
+    public static int sum(int[][] array) {
+        int rsl = 0;
+        for (int[] arr : array) {
+            for (int num : arr) {
+                rsl += num;
+            }
+        }
+        return rsl;
+    }
+}
