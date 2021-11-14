5337d7f15435410ca56df739b8bd9b8ba58c4b72
kai-73 <oroman@rambler.ru>
2021-11-15 01:06:11 +0300

    0.1. Проверить сортировку массива

---
+++ src/main/java/exercises/Sorted.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Sorted {
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
