d38fa398cc92fe9a9f1a1a6b275de943dd0071fe
kai-73 <oroman@rambler.ru>
2021-11-16 23:16:47 +0300

    1.3. Проверить является ли двумерный массив квадратным.

---
+++ src/main/java/exercises/CheckSquareArray13.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class CheckSquareArray13 {
+    public static boolean checkArray(int[][] array) {
+        int standard = array.length;
+        for (int[] ints : array) {
+            if (ints.length != standard) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/exercises/NonSquareArray12.java
+++ src/main/java/exercises/NonSquareArray12.java
@@ -4,18 +4,15 @@ public class NonSquareArray12 {
     public static void main(String[] args) {
         int[][] array = new int[][]{{1}, {2, 3}, {4, 5, 6}};
         for (int i = 0; i < array.length; i++) {
-<<<<<<< HEAD
             System.out.print(array[i].length + " - ");
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
-=======
             System.out.print(array[i].length + " - {");
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(" " + array[i][j] + " ");
             }
             System.out.println("}");
->>>>>>> 4a5e4bf (1.2. Создание неоднородного двумерного массива.)
             System.out.println();
         }
     }
