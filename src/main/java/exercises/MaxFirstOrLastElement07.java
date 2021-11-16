90c31d7327aab15456a80d37ef8a32f28c1838a5
kai-73 <oroman@rambler.ru>
2021-11-16 21:52:47 +0300

    0.7. Максимум из первого и последнего элемента массива

---
+++ src/main/java/exercises/MaxFirstOrLastElement07.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class MaxFirstOrLastElement07 {
+    public static int getMaxValue(int[] array) {
+        int max = Math.max(array[0], array[array.length - 1]);
+        return max;
+    }
+}
+//        int max = array[0] > array[array.length - 1] ? array[0] :array[array.length - 1];
