049338f11aa207111735df5f9e0b6ce2b917d935
kai-73 <oroman@rambler.ru>
2021-11-16 21:20:09 +0300

    0.5. Индексы в массивах

---
+++ src/main/java/exercises/ArrayIndex.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class ArrayIndex {
+    public static int getFirstElement(int[] array) {
+        int rsl = array[0];
+        return rsl;
+    }
+
+    public static int getLastElement(int[] array) {
+        int rsl = array[array.length - 1];
+        return rsl;
+    }
+}
