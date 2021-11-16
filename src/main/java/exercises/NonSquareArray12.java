4904c8b22f9f1581206370649baba0540312918b
kai-73 <oroman@rambler.ru>
2021-11-16 22:51:35 +0300

    1.2. Создание неоднородного двумерного массива.

---
+++ src/main/java/exercises/NonSquareArray12.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class NonSquareArray12 {
+    public static void main(String[] args) {
+        int[][] array = new int[][]{{1}, {2, 3}, {4, 5, 6}};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i].length + " - ");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
