bb735e99113e5a029d36ac476ce81b475bceeb9f
kai-73 <oroman@rambler.ru>
2021-11-16 22:15:36 +0300

    0.9. Размер двумерного массива

---
+++ src/main/java/exercises/Board09.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class Board09 {
+        public static int getCountRow(int[][] array) {
+            return array.length;
+        }
+
+        public static int getCountCellInRow(int[][] array, int row) {
+            return array[row].length;
+        }
+}
