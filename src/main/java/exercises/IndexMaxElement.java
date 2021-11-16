418f7cd949c8676da28ba80b9b2fafc7f24a05cc
kai-73 <oroman@rambler.ru>
2021-11-16 22:01:30 +0300

    0.8. Индекс максимального значения

---
+++ src/main/java/exercises/IndexMaxElement.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class IndexMaxElement {
+    public static int getIndexMax(int[] array) {
+        return array[0] > array[array.length - 1] ? 0 : array.length - 1;
+    }
+}
+
