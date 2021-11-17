8d00ecd634d1a1d58c875017c36c74eeff8c8416
kai-73 <oroman@rambler.ru>
2021-11-17 15:04:42 +0300

    1.8 Транспонировать матрицу

---
+++ src/main/java/exercises/TriangleMatrix17.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class TriangleMatrix17 {
+
+    public static int[][] rows(int count) {
+            int[][] triangle = new int[count][];
+            int k = 1;
+            for (int i = 0; i < triangle.length; i++) {
+                triangle[i] = new int[i + 1]; //создаем одномерный массив строки i, что бы знать размер для trianle[i].length
+                for (int j = 0; j < triangle[i].length; j++) {
+                    triangle[i][j] = k;
+                    k++;
+                }
+            }
+            return triangle;
+    }
+}
