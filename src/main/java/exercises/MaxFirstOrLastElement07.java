5c320e57a109a0b05764d118df56a71ac78b6191
kai-73 <oroman@rambler.ru>
2021-11-21 01:03:13 +0300

    Простое объединение двух массивов

--- src/main/java/exercises/MaxFirstOrLastElement07.java
+++ src/main/java/exercises/MaxFirstOrLastElement07.java
@@ -2,8 +2,7 @@ package exercises;
 
 public class MaxFirstOrLastElement07 {
     public static int getMaxValue(int[] array) {
-        int max = Math.max(array[0], array[array.length - 1]);
-        return max;
+        return Math.max(array[0], array[array.length - 1]);
     }
 }
 //        int max = array[0] > array[array.length - 1] ? array[0] :array[array.length - 1];
