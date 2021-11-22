3c5494307d80953e32af872e1c253a393cf359a0
kai-73 <oroman@rambler.ru>
2021-11-23 01:15:27 +0300

    3.3. Превратить одномерный массив в двумерный.

---
+++ src/main/java/exercises/ArrayInSquareArray33.java
@@ -0,0 +1,48 @@
+package exercises;
+
+public class ArrayInSquareArray33 {
+    public static int[][] convertArray(int[] array) {
+        int size = (int) (Math.ceil(Math.sqrt(array.length)));
+        int[][] rsl = new int[size][size];
+        int i = 0, row = 0, cell = 0;
+        while (i < array.length) {
+            while (cell < size) {
+                rsl[row][cell++] = i < array.length ? array[i++] : 0;
+            }
+            cell = 0;
+            row++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 4, 6, 6, 7};
+        int[][] rsl = convertArray(array);
+        for (int i = 0; i < rsl.length; i++) {
+            for (int j = 0; j < rsl[i].length; j++) {
+                System.out.print(rsl[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+/*
+package ru.job4j.array;
+
+public class ArrayInSquareArray {
+    public static int[][] convertArray(int[] array) {
+       int size = (int) Math.ceil(Math.sqrt(array.length));
+        int[][] rsl = new int[size][size];
+      	int i = 0, row = 0, cell = 0;
+        while (i < array.length) {
+            rsl[row][cell++] = array[i++];
+            if (cell == size) {
+                cell = 0;
+                row++;
+            }
+        }
+        return rsl;
+    }
+}
+ */
