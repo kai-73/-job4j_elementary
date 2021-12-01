cd593baa496982435b1a79cb87cc56d8370424fa
kai-73 <oroman@rambler.ru>
2021-12-01 03:50:24 +0300

    5.3. Разность множеств

--- src/main/java/exercises/arrays/OrArray52.java
+++ src/main/java/exercises/arrays/OrArray52.java
@@ -53,4 +53,18 @@ public class OrArray52 {
         return Arrays.copyOf(rsl, count);
     }
 }
-
+/*
+ public static int[] or(int[] left, int[] right) {
+    int[] rsl = left.length > 0 ? left : right;
+        int count = rsl.length;
+        for (int i = 0; i < left.length; i++) {
+            for (int j = 0; j < right.length; j++) {
+                if (rsl[i] != right[j] && Arrays.binarySearch(rsl, right[j]) < -1) {
+                  rsl = Arrays.copyOf(rsl, ++count);
+                  rsl[count - 1] = right[j];
+                }
+            }
+        }
+        return rsl;
+    }
+ */
