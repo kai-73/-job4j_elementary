97947a43a392295fb4e673a365c579412d2c39a7
kai-73 <oroman@rambler.ru>
2021-12-07 22:49:34 +0300

    5.7. split

---
+++ src/test/java/exercises/arrays/SplitTest.java
@@ -0,0 +1,24 @@
+package exercises.arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SplitTest {
+
+    @Test
+    public void test() {
+        assertArrayEquals(new String[] {"one", "two"}, convert(Split57.split("one two".toCharArray(), ' ')));
+        assertArrayEquals(new String[] {"one", "two", "three"}, convert(Split57.split("one two three".toCharArray(), ' ')));
+        assertArrayEquals(new String[] {"onetwothree"}, convert(Split57.split("onetwothree".toCharArray(), ' ')));
+    }
+
+    private String[] convert(char[][] splitted) {
+        String[] result = new String[splitted.length];
+        int ri = 0;
+        for (char[] row : splitted) {
+            result[ri++] = new String(row);
+        }
+        return result;
+    }
+}
