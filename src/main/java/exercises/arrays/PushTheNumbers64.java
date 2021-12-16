9f9c5b40997cbf73e351cd6bc1f1b0174505161d
kai-73 <oroman@rambler.ru>
2021-12-16 23:02:53 +0300

    6.4. Раздвинуть элементы массива

--- src/main/java/exercises/arrays/PushTheNumbers64.java
+++ src/main/java/exercises/arrays/PushTheNumbers64.java
@@ -1,29 +1,37 @@
 package exercises.arrays;
 
+/**
+ * В данной задаче вам предстоит реализовать алгоритм сдвига элементов массива.
+ * Вы должны написать метод который принимает в качестве параметров двумерный
+ * массив(количество рядов должно быть равно количеству строк),
+ * массив заполнен случайными числами от 1 до 100,
+ * и два индекса
+ * индекс строки и индекс колонки.
+ * В данную ячейку записывается 0 а все цифры левее, правее, выше и ниже сдвигаются на 1 ячейку
+ * Например было
+ * {1, 2, 3},
+ * {4, 5, 6},
+ * {7, 8, 9} и если индексы это 1 и 1 то массив должен иметь вид
+ * {1, 5, 3},
+ * {5, 0, 5},
+ * {7, 5, 9}
+ */
 public class PushTheNumbers64 {
 
     public static void push(int[][] array, int row, int column) {
-       //  for (int i = 0; i < array.length; i++) {
-        //    for (int j = 0; j < array[i].length; j++) {
-            //    if (i == row && j == column - 1) {
+
                     for (int k = 0; k < column; k++) {
                         array[row][k] = array[row][k + 1];
                     }
-             //   } else if (i == row && j == column + 1) {
-                    for (int k = 0; k < array.length - column; k++) {
+                    for (int k = 0; k < array.length - column - 1; k++) {
                        array[row][array.length - 1 - k] = array[row][array.length - 2 - k];
                    }
-              //  } else if (i == row - 1 && j == column) {
                     for (int k = 0; k < row; k++) {
                         array[k][column] = array[k + 1][column];
                     }
-               // } else if (row + 1 == i && column == j) {
-                    for (int k = 0; k <  array.length - row; k++) {
+                    for (int k = 0; k <  array.length - row - 1; k++) {
                         array[array.length - 1 - k][column] = array[array.length - 2 - k][column];
                     }
-               // }
-           // }
-        //}
         array[row][column] = 0;
     }
 }
