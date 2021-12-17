5db2b64fed35ca86a5b1e398e1d555d708e56dcc
kai-73 <oroman@rambler.ru>
2021-12-18 00:24:26 +0300

    Чистка очереди Коммита

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,14 +1,13 @@
 package ru.job4j;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 public class MainTest {
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
         int result = 1;
-        assertThat(result, is(excepted));
+        Assert.assertEquals(result, excepted);
     }
 }
